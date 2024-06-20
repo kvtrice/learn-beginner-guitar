76d28e5ca78291af384d36fefa96dfd62cd66061
kvtrice <katricemountford@gmail.com>
2024-06-20 17:18:20 +1000

    Added hero and header styling

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const header = document.querySelector(".header");
+
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 60) {
+			header.classList.add("scrolled");
+		} else {
+			header.classList.remove("scrolled");
+		}
+	});
+});
