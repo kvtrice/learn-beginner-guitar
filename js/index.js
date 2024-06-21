3eaa9ba0fa9ba77ed873856bcea6501a29bf2b5c
kvtrice <katricemountford@gmail.com>
2024-06-21 23:43:32 +1000

    Added navigation tiles to explore section

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const header = document.querySelector(".header");
+	const header = document.querySelector(".header__container");
+	const linkText = document.querySelectorAll(".header__nav a");
+	const titleText = document.querySelector(".header__logo a");
 
 	window.addEventListener("scroll", () => {
 		if (window.scrollY > 60) {
 			header.classList.add("scrolled");
+			titleText.classList.add("scrolled--text");
+
+			linkText.forEach(link => {
+				link.classList.add("scrolled--text");
+			});
 		} else {
 			header.classList.remove("scrolled");
+			titleText.classList.remove("scrolled--text");
+			linkText.forEach(link => {
+				link.classList.remove("scrolled--text");
+			});
 		}
 	});
 });
