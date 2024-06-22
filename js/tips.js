f40cb88cf5e2ffe354a5e2a00b6878c2cecc4600
kvtrice <katricemountford@gmail.com>
2024-06-22 21:53:59 +1000

    Adds collapsible tips to tips page

---
+++ js/tips.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const tips = document.querySelectorAll(".tip");
+
+	tips.forEach(tip => {
+		tip.querySelector(".tip__title").addEventListener("click", () => {
+			const content = tip.querySelector(".tip__content");
+			content.style.display =
+				content.style.display === "block" ? "none" : "block";
+		});
+	});
+});
